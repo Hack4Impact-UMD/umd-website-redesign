55431f97c7312f2d71251d9b18b6f7f8fbf2a957
andrew <40076917+a-ys@users.noreply.github.com>
2022-03-09 03:00:27 -0500

    Create person profile component

--- umd-website-redesign/src/App.tsx
+++ umd-website-redesign/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
       <Footer />
+      <Person memberName={'Andrew Song'} team={'Website Redesign Team'} role={'Developer'} pronouns={'he/him'} />
     </div>
   );
 }
