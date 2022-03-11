e90768eb5bce70196deb2291e0f523bf797a70a8
Matthee Sabulao <matthee@matthees-mbp.myfiosgateway.com>
2022-03-10 23:10:28 -0500

    Navbar Component
    Adding first version of navbar component files

---
+++ umd-website-redesign/src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { MenuItems } from "./MenuItems"
+import './Navbar.css'
+import h4iLogo from '../h4i-logo.svg'
+
+class Navbar extends Component {
+
+    render(){
+        //Map will go through each component and present them in a list  (sets up the alignment)
+
+        //menu-icon is for later use for mobile implementation
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo"></h1>
+                <div className="menu-icon"> 
+                    <img className="logo" src={h4iLogo}></img>
+                </div>
+                <ul className={"nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
