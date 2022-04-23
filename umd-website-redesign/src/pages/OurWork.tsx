7a173e7b93cab02442696457b04c807dc4c8bbea
Eric Yi <ericyi2001@gmail.com>
2022-04-23 04:07:35 -0400

    Implemented button to scroll to top of page

--- umd-website-redesign/src/pages/OurWork.tsx
+++ umd-website-redesign/src/pages/OurWork.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OurWorkHeader from '../components/our_work/OurWorkHeader';
 import CurrentProjects from '../components/our_work/CurrentProjects';
 import PastProjects from '../components/our_work/PastProjects';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer';
 import styles from '../styles/our_work/OurWork.module.css';
+//import { ReactComponent as HeaderBackground } from '../components/assets/bluewave_desktop.svg';
+import ScrollToTopButton from '../components/assets/top of page button.jpg';
 
 const OurWork: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
   return (
-    <div>
+    <div id={styles.OurWorkPage}>
+      {/*<HeaderBackground id={styles.headerBackground} />*/}
       <div className={styles.navBarDiv}>
         <Navbar />
       </div>
       <OurWorkHeader />
       <CurrentProjects />
       <PastProjects />
+      <img
+        id={styles.ScrollToTopButton}
+        src={ScrollToTopButton}
+        alt="Scroll to Top of Page"
+        onClick={scrollToTop}
+        style={{ display: visible ? 'inline' : 'none' }}
+      />
       <Footer />
     </div>
   );
