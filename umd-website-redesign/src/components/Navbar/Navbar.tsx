d6d23c0f0f8f822c23b761d49b1261b22f20fa23
Matt Sabulao <mattsa9@gmail.com>
2022-04-25 15:38:43 -0400

    Made navbar functional component

--- umd-website-redesign/src/components/Navbar/Navbar.tsx
+++ umd-website-redesign/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
 import h4iLogo from './logo.svg';
 import bars from  './bars-solid.svg';
-class Navbar extends Component {
+import { getValue } from '@testing-library/user-event/dist/utils';
+const Navbar = () => {
 
-    state = { clicked: false}
+    const[currState, setState] = useState(true);
 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
+    const handleState = () => {
+        setState(!currState);
     }
 
-    render(){
         //Map will go through each component and present them in a list  (sets up the alignment)
 
         //menu-icon is for later use for mobile implementation
@@ -20,11 +20,11 @@ class Navbar extends Component {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <h1 className="navbar-logo"></h1>
                 <img className="logo" src={h4iLogo}></img>
-                <div className="menu-icon" onClick={this.handleClick}> 
-                    <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
+                <div className="menu-icon" onClick={handleState}> 
+                    <i className={currState ? 'fa fa-times' : 'fa fa-bars'}></i>
                     
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                <ul className={currState ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={item.cName}>
@@ -37,7 +37,7 @@ class Navbar extends Component {
                 </ul>
             </nav>
         )
-    }
+    
 }
 
 export default Navbar
