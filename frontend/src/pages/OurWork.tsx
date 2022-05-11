2f281ba7aa9c17d971988a77f7a9554218161557
Eric Yi <ericyi2001@gmail.com>
2022-05-11 08:14:49 -0400

    Updated scroll to top of page button

--- frontend/src/pages/OurWork.tsx
+++ frontend/src/pages/OurWork.tsx
@@ -5,8 +5,9 @@ import PastProjects from '../components/our_work/PastProjects';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import styles from '../styles/our_work/OurWork.module.css';
-//import { ReactComponent as HeaderBackground } from '../components/assets/bluewave_desktop.svg';
-import ScrollToTopButton from '../components/assets/top of page button.jpg';
+import { ReactComponent as HeaderBackground } from '../components/assets/bluewave_desktop.svg';
+import ScrollToTopButton from '../components/assets/ScrollToTopButton.png';
+import ScrollToTopButtonHover from '../components/assets/ScrollToTopButtonHover.png';
 
 const OurWork: React.FC = () => {
   const [visible, setVisible] = useState(false);
@@ -31,20 +32,25 @@ const OurWork: React.FC = () => {
 
   return (
     <div id={styles.OurWorkPage}>
-      {/*<HeaderBackground id={styles.headerBackground} />*/}
       <div className={styles.navBarDiv}>
         <Navbar />
       </div>
-      <OurWorkHeader />
-      <CurrentProjects />
-      <PastProjects />
-      <img
-        id={styles.ScrollToTopButton}
-        src={ScrollToTopButton}
-        alt="Scroll to Top of Page"
-        onClick={scrollToTop}
-        style={{ display: visible ? 'inline' : 'none' }}
-      />
+      {/*<div className={styles.headerBackground}>
+        <HeaderBackground />
+        </div>*/}
+      <div className={styles.content}>
+        <OurWorkHeader />
+        <CurrentProjects />
+        <PastProjects />
+      </div>
+      <div className={styles.ScrollToTopButton} onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}>
+        <img src={ScrollToTopButton} aria-label="Scroll to Top Button" />
+        <img
+          className={styles.ScrollToTopButtonHover}
+          src={ScrollToTopButtonHover}
+          aria-label="Scroll to Top Button Hover"
+        />
+      </div>
       <Footer />
     </div>
   );
