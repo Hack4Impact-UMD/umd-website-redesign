c199f3f9287e6fb782b86fd25eeacd4cbc9a48e1
andrew <40076917+a-ys@users.noreply.github.com>
2022-06-14 00:30:34 -0400

    Add links between apply pages. Update routes.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
-// importing pages 
+// importing pages
 import App from './App';
 import AboutUs from './pages/AboutUs';
 import Navbar from './components/navbar/Navbar';
 import StudentApply from './pages/StudentApply';
+import NonprofitApply from './pages/NonprofitApply';
 import Footer from './components/footer/Footer';
 import OurWork from './pages/OurWork';
 
 const routing = (
   <BrowserRouter>
     <div>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="/aboutus" element={<AboutUs/>} />
-        <Route path="/ourwork" element={<OurWork/>} />
-        <Route path="/apply" element={<StudentApply/>} />
-        <Route path="/contactus" element={<App/>} />
+        <Route path="/" element={<App />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/ourwork" element={<OurWork />} />
+        <Route path="/apply" element={<StudentApply />} />
+        <Route path="/apply/student" element={<StudentApply />} />
+        <Route path="/apply/nonprofit" element={<NonprofitApply />} />
+        <Route path="/contactus" element={<App />} />
       </Routes>
-    <Footer/>
+      <Footer />
     </div>
   </BrowserRouter>
 );
