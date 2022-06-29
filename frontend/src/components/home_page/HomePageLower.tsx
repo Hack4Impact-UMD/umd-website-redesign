a8221375c86f4ec4f9aa0eefc57980461ab42382
andrew <40076917+a-ys@users.noreply.github.com>
2022-06-29 04:18:39 -0400

    Reformat homepage, featuredprojects files. Separated into another commit to avoid unclear diffs :D

--- frontend/src/components/home_page/HomePageLower.tsx
+++ frontend/src/components/home_page/HomePageLower.tsx
@@ -8,19 +8,23 @@ const HomePageLower = () => {
       <div id={styles.studentsContainer}>
         <h1>Students</h1>
         <p>
-          Our members consist of current University of Maryland students interested in using tech for social good. Each semester, members are split into teams of Product Managers, UI/UX Designers, and Engineers and partner with a nonprofit organization to develop a product that will help the organization.
+          Our members consist of current University of Maryland students interested in using tech for social good. Each
+          semester, members are split into teams of Product Managers, UI/UX Designers, and Engineers and partner with a
+          nonprofit organization to develop a product that will help the organization.
         </p>
         <div className={styles.bluebtn}>
-        <OblongButton text="Join Us" color="blue" link="/apply/student" />
+          <OblongButton text="Join Us" color="blue" link="/apply/student" />
         </div>
       </div>
       <div id={styles.nonprofitsContainer}>
         <h1>Nonprofits</h1>
         <p>
-          Nonprofit organizations are the pillars of our community; they focus on a variety of social issues, and Hack4Impact-UMD focuses on helping these organizations with any of their technical needs to improve the effectiveness of their work. 
+          Nonprofit organizations are the pillars of our community; they focus on a variety of social issues, and
+          Hack4Impact-UMD focuses on helping these organizations with any of their technical needs to improve the
+          effectiveness of their work.
         </p>
         <div className={styles.bluebtn}>
-        <OblongButton text="Work With Us" color="blue" link="/apply/nonprofit" />
+          <OblongButton text="Work With Us" color="blue" link="/apply/nonprofit" />
         </div>
       </div>
     </div>
--- frontend/src/components/home_page/HomePageTop.tsx
+++ frontend/src/components/home_page/HomePageTop.tsx
@@ -7,24 +7,30 @@ import OblongButton from '../buttons/OblongButton';
 const HomePageTop = () => {
   return (
     <div>
-        <img className={styles.blueBG} src={blueBG}></img>
+      <img className={styles.blueBG} src={blueBG}></img>
       <div className={styles.top}>
         <div className={styles.textOne}>
           <p className={styles.textOne}>
-          Building powerful nonprofit software as a tool for <span className={styles.socialGoodSubText}>social good</span>
+            Building powerful nonprofit software as a tool for{' '}
+            <span className={styles.socialGoodSubText}>social good</span>
           </p>
         </div>
         <p className={styles.textTwo}>
-        We are a student-run organization that empowers engineers, designers, activists, and humanitarians to create lasting social change.
+          We are a student-run organization that empowers engineers, designers, activists, and humanitarians to create
+          lasting social change.
         </p>
         <div className={styles.mintbtn}>
-        <OblongButton text="Learn More" color="green" link="/aboutus"/>
+          <OblongButton text="Learn More" color="green" link="/aboutus" />
         </div>
         <img className={styles.graphic} src={graphic}></img>
       </div>
       <div className={styles.center}>
         <header className={styles.featuredTitle}>Featured Projects</header>
-        <p className={styles.featuredDescription}>We are the University of Maryland, College Park chapter of the national Hack4Impact organization, a student-run 501(c)(3) dedicated to building software for social impact. Each year, we partner with nonprofits to help them better serve their communities.</p>
+        <p className={styles.featuredDescription}>
+          We are the University of Maryland, College Park chapter of the national Hack4Impact organization, a
+          student-run 501(c)(3) dedicated to building software for social impact. Each year, we partner with nonprofits
+          to help them better serve their communities.
+        </p>
       </div>
     </div>
   );
