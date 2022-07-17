187772d1a26f0f0c8bd13d4f22a650194589459b
andrew <40076917+a-ys@users.noreply.github.com>
2022-07-17 05:30:28 -0400

    Make App.tsx, index.tsx prettier.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,13 +15,13 @@ import OurWork from './pages/OurWork';
 import ProjectPage from './pages/ProjectPage';
 
 // https://stackoverflow.com/a/70194027
-const ScrollToTopWrapper = ({children}: {children: JSX.Element}) => {
-    const location = useLocation();
-    useLayoutEffect(() => {
-        document.documentElement.scrollTo(0, 0);
-    }, [location.pathname]);
-    return (children);
-}
+const ScrollToTopWrapper = ({ children }: { children: JSX.Element }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 
 function Homepage() {
   return (
@@ -34,26 +34,26 @@ function Homepage() {
 }
 
 function App() {
-    return (
-      <BrowserRouter>
-    <ScrollToTopWrapper>
+  return (
+    <BrowserRouter>
+      <ScrollToTopWrapper>
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Homepage/>} />
-            <Route path="/aboutus" element={<AboutUs/>} />
-            <Route path="/ourwork" element={<OurWork/>} />
-            <Route path="/apply" element={<StudentApply/>} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/ourwork" element={<OurWork />} />
+            <Route path="/apply" element={<StudentApply />} />
             <Route path="/apply/student" element={<StudentApply />} />
             <Route path="/apply/nonprofit" element={<NonprofitApply />} />
-            <Route path="/contactus" element={<App/>} />
-            <Route path="ourwork/:projectpath" element={<ProjectPage/>} />
+            <Route path="/contactus" element={<App />} />
+            <Route path="ourwork/:projectpath" element={<ProjectPage />} />
           </Routes>
           <Footer />
         </div>
-    </ScrollToTopWrapper>
-      </BrowserRouter>
-    );
+      </ScrollToTopWrapper>
+    </BrowserRouter>
+  );
 }
 
 export default App;
