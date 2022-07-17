c505206b7871b25dbe015627608b9dd21c3ebe9f
andrew <40076917+a-ys@users.noreply.github.com>
2022-07-17 05:29:03 -0400

    Scroll to top on page change. Refactor app/index.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { Route, BrowserRouter, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// importing pages
 import HomePageTop from './components/home_page/HomePageTop';
 import HomePageLower from './components/home_page/HomePageLower';
 import FeaturedProjects from './components/home_page/FeaturedProjects';
+import AboutUs from './pages/AboutUs';
+import Navbar from './components/navbar/Navbar';
+import StudentApply from './pages/StudentApply';
+import NonprofitApply from './pages/NonprofitApply';
+import Footer from './components/footer/Footer';
+import OurWork from './pages/OurWork';
+import ProjectPage from './pages/ProjectPage';
 
+// https://stackoverflow.com/a/70194027
+const ScrollToTopWrapper = ({children}: {children: JSX.Element}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+        document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return (children);
+}
 
-function App() {
+function Homepage() {
   return (
-    <div className="App">
+    <div>
       <HomePageTop />
       <FeaturedProjects />
       <HomePageLower />
     </div>
   );
+}
+
+function App() {
+    return (
+      <BrowserRouter>
+    <ScrollToTopWrapper>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage/>} />
+            <Route path="/aboutus" element={<AboutUs/>} />
+            <Route path="/ourwork" element={<OurWork/>} />
+            <Route path="/apply" element={<StudentApply/>} />
+            <Route path="/apply/student" element={<StudentApply />} />
+            <Route path="/apply/nonprofit" element={<NonprofitApply />} />
+            <Route path="/contactus" element={<App/>} />
+            <Route path="ourwork/:projectpath" element={<ProjectPage/>} />
+          </Routes>
+          <Footer />
+        </div>
+    </ScrollToTopWrapper>
+      </BrowserRouter>
+    );
 }
 
 export default App;
