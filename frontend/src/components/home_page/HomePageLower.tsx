ee5164cfcea9bbeb5f4786e8bee8754d671604a8
Eric Yi <ericyi2001@gmail.com>
2022-07-17 21:00:19 -0400

    Fix: make Home Page buttons vertically aligned
    Updated the component for the lower part of the Home Page and the CSS
    styling for the component to make Home Page buttons vertically aligned
    in desktop layout:
    - Put text and buttons in separate divs in order to make sure buttons
    stick to bottom of the student and nonprofits containers.
    - Added flex-direction and flex-grow attributes to CSS file and made the
    textHolder class always take up the full height of the div minus the
    height of the button div to make sure that button does not overlap with
    text elements.
    - Also cleaned up CSS a little for the mobile view to make margins and
    padding easier to see.

--- frontend/src/components/home_page/HomePageLower.tsx
+++ frontend/src/components/home_page/HomePageLower.tsx
@@ -5,26 +5,38 @@ const HomePageLower = () => {
   return (
     <div id={styles.parentContainer}>
       <div id={styles.studentsContainer}>
-        <h1>Students</h1>
-        <p>
-          Our members consist of current University of Maryland students interested in using tech for social good. Each semester, members are split into teams of Product Managers, UI/UX Designers, and Engineers and partner with a nonprofit organization to develop a product that will help the organization.
-        </p>
-        <a href='/apply/student'>
-          <button aria-label="Join Us" className={styles.bluebtn}>
-            Join Us
-          </button>
-        </a>
+        <div className={styles.textHolder}>
+          <h1>Students</h1>
+          <p>
+            Our members consist of current University of Maryland students interested in using tech for social good.
+            Each semester, members are split into teams of Product Managers, UI/UX Designers, and Engineers and partner
+            with a nonprofit organization to develop a product that will help the organization.
+          </p>
+        </div>
+        <div className={styles.buttonHolder}>
+          <a href="/apply/student">
+            <button aria-label="Join Us" className={styles.bluebtn}>
+              Join Us
+            </button>
+          </a>
+        </div>
       </div>
       <div id={styles.nonprofitsContainer}>
-        <h1>Nonprofits</h1>
-        <p>
-          Nonprofit organizations are the pillars of our community; they focus on a variety of social issues, and Hack4Impact-UMD focuses on helping these organizations with any of their technical needs to improve the effectiveness of their work. 
-        </p>
-        <a href='/apply/nonprofit'>
-          <button aria-label="Work With Us" className={styles.bluebtn}>
-            Work With Us
-          </button>
-        </a>
+        <div className={styles.textHolder}>
+          <h1>Nonprofits</h1>
+          <p>
+            Nonprofit organizations are the pillars of our community; they focus on a variety of social issues, and
+            Hack4Impact-UMD focuses on helping these organizations with any of their technical needs to improve the
+            effectiveness of their work.
+          </p>
+        </div>
+        <div className={styles.buttonHolder}>
+          <a href="/apply/nonprofit">
+            <button aria-label="Work With Us" className={styles.bluebtn}>
+              Work With Us
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
