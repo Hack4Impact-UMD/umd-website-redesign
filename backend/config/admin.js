d82cd766beb5a8cbc1dbc3c13c0e6dcc6a084639
Surabi Ramamurthy <sramamur@umd.edu>
2022-07-20 19:29:05 -0500

    setting up backend

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'JY/94R4V3cPcKMdIZfMQtzf3KaNXwG7lUxd9y/4id1Y='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'JGT4V99lBVL/WwzaKGkNF8ii5ZjMqoiIs4WxFTN/BT0='),
   },
 });
--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['otf/LzCphqGi+P97We8U6FDaPtWqYFuR7aAJeF7j1iM='], ),
   },
 });
