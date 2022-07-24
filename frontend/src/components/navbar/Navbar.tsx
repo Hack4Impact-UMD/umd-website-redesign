3bd160a754a0d43de741e29b9f342c6087843f62
Rae <rachelw@terpmail.umd.edu>
2022-07-24 11:06:42 -0400

    Styled Navbar Without Blur Background, but with Solid Background
    - New div blurOverlay made for solid background overlay that pops up when menu icon
      is clicked on mobile
    - Also cleaned up the styles a bit (by general nav styles, grouping positional styling
      vs font styling, etc) so they're more readable
    - Styled the active Navbar menu
         * have not got the blurred background working-- this is because it
           requires the contents to be blurred are in the container. unsure
           how to do this from module. Since blur is not working, there is a
           solid background for now
         * Remaining headers done according to figma
         * Re did positioning on logo to have left: value. This is because
           the logo was misaligned in the first place
         * margins/padding for the navMenu items are relative to screen
           width-- this is because the screen's width is so small it should
           play a role in the size of the padding.
    
    - Currently the change in styling when the menu icon is clicked is done
      with react handleState. Styles conditionally applied in Navbar.tsx
      depending on whether the menu is enabled or not
           * Future consideration to css-only styling, or a more light
    	 weight way to do a hamburger menu are under way
           * Currently I have written the navbar this way for swiftness only
    - Bugs:
         - transition has vestige of dropdown container styling. Likely has
           to do with the way switching between menu bar states is done
         - transition to hide menu is not done
         - After making the menu active, and resizing the window to be
           larger than the media query, link styling does not return.

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -13,46 +13,49 @@ const Navbar = () => {
 
   //menu-icon is for later use for mobile implementation
   return (
-    <nav className={styles.navbarItems}>
-      <Link to="/">
-        <img alt="Hack4Impact Logo" className={styles.logo} src={h4iLogo}></img>
-      </Link>
-      <img className={styles.navIcon} src={Hamburger} onClick={handleState}></img>
-      <ul className={currState ? styles.navMenu.active : styles.navMenu}>
-        <li>
-          <Link className={styles.navLinks} to={'/aboutus'}>
-            {'About Us'}
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.navLinks} to={'/ourwork'}>
-            {'Our Work'}
-          </Link>
-        </li>
-        <li>
-            <Link className={styles.navLinks + " " + styles.applyDropdownButton} to={'/apply/student'}>Apply</Link>
-            <div className={styles.applyDropdownContainer}>
-              <ul className={styles.applyDropdownContent}>
-                <li>
-                  <Link className={styles.navLinks} to={'/apply/student'}>
-                  {'For Students'}
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles.navLinks} to={'/apply/nonprofit'}>
-                    {'For Nonprofits'}
-                  </Link>
-                </li>
-              </ul>
-            </div>
-        </li>
-        <li>
-          <a className={styles.navLinks} href={'mailto:umd@hack4impact.org'}>
-            {'Contact Us'}
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <div className={currState ? styles.blurOverlay : ''}></div> 
+      <nav className={styles.navbarItems}>
+        <Link to="/">
+          <img alt="Hack4Impact Logo" className={styles.logo} src={h4iLogo}></img>
+        </Link>
+        <img className={styles.navIcon} src={Hamburger} onClick={handleState}></img>
+        <ul className={currState ? styles.navMenu.active : styles.navMenu}>
+          <li>
+            <Link className={styles.navLinks} to={'/aboutus'}>
+              {'About Us'}
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.navLinks} to={'/ourwork'}>
+              {'Our Work'}
+            </Link>
+          </li>
+          <li>
+              <Link className={styles.navLinks + " " + styles.applyDropdownButton} to={'/apply/student'}>Apply</Link>
+              <div className={styles.applyDropdownContainer}>
+                <ul className={styles.applyDropdownContent}>
+                  <li>
+                    <Link className={styles.navLinks} to={'/apply/student'}>
+                    {'For Students'}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className={styles.navLinks} to={'/apply/nonprofit'}>
+                      {'For Nonprofits'}
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+          </li>
+          <li>
+            <a className={styles.navLinks} href={'mailto:umd@hack4impact.org'}>
+              {'Contact Us'}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
