594fad13bfb9f4fa588debbd0342814b6abebcfa
andrew <40076917+a-ys@users.noreply.github.com>
2022-07-27 03:23:37 -0400

    Fix value card styling + animations. Used approach from RoleCard: have front of card be one div, and back of card be another. Then toggle opacity of the two divs when hovering over the parent ValueCard div.

--- frontend/src/components/about_us/ValueCard.tsx
+++ frontend/src/components/about_us/ValueCard.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import styles from '../../styles/about_us/ValueCard.module.css';
-import bg1 from '../assets/ValueCardBackgrounds/ValueCardBackground1.svg';
-import bg2 from '../assets/ValueCardBackgrounds/ValueCardBackground2.svg';
-import bgHover1 from '../assets/ValueCardBackgrounds/ValueCardBackgroundHover1.svg';
-import bgHover2 from '../assets/ValueCardBackgrounds/ValueCardBackgroundHover2.svg';
-
 /*
  * ValueCard Component used in AboutUs page.
- *
- * TODO: Use content div + padding rather than setting width of text boxes? See
- * RoleCard.
  *
  * Props:
  * revBackground (Boolean): if true, gradient is from blue to green.
@@ -19,26 +11,26 @@ import bgHover2 from '../assets/ValueCardBackgrounds/ValueCardBackgroundHover2.s
  */
 function ValueCard(props: any) {
   let valueCardClass: string;
-  let bgMain: string;
-  let bgHover: string;
   if (props.revBackground) {
     /* add additional rev class for mobile styling */
     valueCardClass = `${styles.valueCard} ${styles.rev}`;
-    /* img src links for desktop styling */
-    bgMain = bg2;
-    bgHover = bgHover2;
   } else {
-    bgMain = bg1;
-    bgHover = bgHover1;
     valueCardClass = styles.valueCard;
   }
 
   return (
     <div className={valueCardClass}>
-      <div className={styles.valueCardTextMain}><h2 className={"alternateHeader"}>{props.mainText}</h2></div>
-      <div className={styles.valueCardTextSub}>{props.hoverText}</div>
-      <img src={bgMain} className={styles.valueCardBg1} />
-      <img src={bgHover} className={styles.valueCardBg2} />
+      <div className={styles.cardFront}>
+          <div className={styles.cardContentWrapper}>
+              <div className={styles.valueCardTextMain}><h2 className={"alternateHeader"}>{props.mainText}</h2></div>
+          </div>
+      </div>
+
+      <div className={styles.cardBack}>
+          <div className={styles.cardContentWrapper}>
+              <div className={styles.valueCardTextSub}>{props.hoverText}</div>
+          </div>
+      </div>
     </div>
   );
 }
