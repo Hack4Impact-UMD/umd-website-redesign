8f99b71f1e609ca35dfaee3995eb06afa0756ea4
andrew <40076917+a-ys@users.noreply.github.com>
2022-08-03 01:56:36 -0400

    Add 404 page

---
+++ frontend/src/pages/PageNotFound.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/PageNotFound.module.css';
+
+function PageNotFound() {
+  return <div className={styles.PageNotFound}>404: This page could not be found</div>;
+}
+
+export default PageNotFound;
