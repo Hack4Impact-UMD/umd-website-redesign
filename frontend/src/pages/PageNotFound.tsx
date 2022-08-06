4de853b57d1ccfec5d5fbee9ec3002b37a9f8664
Rae <rachelw@terpmail.umd.edu>
2022-08-06 15:44:41 -0400

    Changed styling of 404 page text to be h1
    - Made the message into a h1 element

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/PageNotFound.module.css';
 
 function PageNotFound() {
-  return <div className={styles.PageNotFound}>404: This page could not be found</div>;
+  return <div className={styles.PageNotFound}><h1>404: This page could not be found</h1></div>;
 }
 
 export default PageNotFound;
