536aa742dc09b4867b6956877be36969099259ef
andrew <40076917+a-ys@users.noreply.github.com>
2022-08-09 04:37:12 -0400

    Fix mobile navbar apply sublinks The links were previously clickable while invisible when the navbar was closed. Other changes:     - rename variables associated with navbar state     - fix active classname of navMenu.

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -5,22 +5,22 @@ import h4iLogo from '../assets/h4i_files/h4i_logo.svg';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [currState, setState] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-  const handleState = () => {
-    setState(!currState);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   //menu-icon is for later use for mobile implementation
   return (
     <div>
-      <div className={currState ? styles.blurOverlay : ''}></div> 
+      <div className={isMenuOpen ? styles.blurOverlay : ''}></div>
       <nav className={styles.navbarItems}>
         <Link to="/">
           <img alt="Hack4Impact Logo" className={styles.logo} src={h4iLogo}></img>
         </Link>
-        <img className={styles.navIcon} src={Hamburger} onClick={handleState}></img>
-        <ul className={currState ? styles.navMenu.active : styles.navMenu}>
+        <img className={styles.navIcon} src={Hamburger} onClick={toggleMenu}></img>
+        <ul className={isMenuOpen ? styles.navMenuActive : styles.navMenu}>
           <li>
             <Link className={styles.navLinks} to={'/aboutus'}>
               {'About Us'}
@@ -33,7 +33,7 @@ const Navbar = () => {
           </li>
           <li>
               <Link className={styles.navLinks + " " + styles.applyDropdownButton} to={'/apply/student'}>Apply</Link>
-              <div className={styles.applyDropdownContainer}>
+              <div className={isMenuOpen ? `${styles.applyDropdownContainer} ${styles.menuOpen}` : styles.applyDropdownContainer}>
                 <ul className={styles.applyDropdownContent}>
                   <li>
                     <Link className={styles.navLinks} to={'/apply/student'}>
