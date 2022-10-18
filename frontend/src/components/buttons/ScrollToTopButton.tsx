c0d4027372c6bb003634513c4c6557f12489db49
huamanie <edwinhuamani@edwins-mbp.mynetworksettings.com>
2022-10-17 21:43:43 -0400

    resize the scrollToTopButton for mobile mode

--- frontend/src/components/buttons/ScrollToTopButton.tsx
+++ frontend/src/components/buttons/ScrollToTopButton.tsx
@@ -27,11 +27,17 @@ const ScrollToTopButton: React.FC = () => {
 
   return (
     <div className={styles.ScrollToTopButton} onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}>
-      <img src={ScrollToTopButtonImg} aria-label="Scroll to Top Button" />
+      <img 
+        src={ScrollToTopButtonImg} 
+        aria-label="Scroll to Top Button" 
+        width={window.innerWidth <= 500 ? 59 : 64} // Check if in mobile mode
+        height={window.innerWidth <= 500 ? 59 : 64}/>
       <img
         className={styles.ScrollToTopButtonHover}
         src={ScrollToTopButtonHoverImg}
         aria-label="Scroll to Top Button Hover"
+        width={window.innerWidth <= 500 ? 59 : 64}
+        height={window.innerWidth <= 500 ? 59 : 64}
       />
     </div>
   );
