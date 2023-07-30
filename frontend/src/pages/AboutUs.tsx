37fcf57936d4ecec370687a1932d854e991bc1ac
Sahil Gaba <sgaba9778@gmail.com>
2023-07-30 15:19:11 -0400

    Fixed bug to show all members

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -41,18 +41,26 @@ function OurMission() {
     <div className={styles.ourMission}>
       <h2>Our Mission</h2>
       <p>
-        Hack4Impact-UMD is a student organization at the University of Maryland, College Park. Founded in Fall 2020 by <a href="https://www.linkedin.com/in/lydia-hu/">Lydia Hu</a>, <a href="https://www.linkedin.com/in/simin-li-88088b/">Simin Li</a>, and <a href="https://www.linkedin.com/in/abbie-tran-a47893153/">Abbie Tran</a>, the club focuses on using tech skills for helping the community while introducing students to a professional working environment and other post-graduation options compared to industry and academia.
+        Hack4Impact-UMD is a student organization at the University of Maryland, College Park. Founded in Fall 2020 by{' '}
+        <a href="https://www.linkedin.com/in/lydia-hu/">Lydia Hu</a>,{' '}
+        <a href="https://www.linkedin.com/in/simin-li-88088b/">Simin Li</a>, and{' '}
+        <a href="https://www.linkedin.com/in/abbie-tran-a47893153/">Abbie Tran</a>, the club focuses on using tech
+        skills for helping the community while introducing students to a professional working environment and other
+        post-graduation options compared to industry and academia.
       </p>
     </div>
   );
 }
 
 function ValueCardRow() {
-  const beOpenMinded = "Our process depends on openness to different people, topics, and perspectives. We embrace difference and work against intolerance to foster an inclusive environment. Our goal is to expose our members to the vast opportunities and daunting challenges in our work.";
+  const beOpenMinded =
+    'Our process depends on openness to different people, topics, and perspectives. We embrace difference and work against intolerance to foster an inclusive environment. Our goal is to expose our members to the vast opportunities and daunting challenges in our work.';
 
-  const goBeyondTechnology = "Technology is only one tool we use in our greater mission for social impact. Technology alone is not enough. We learn from, work with, and are inspired by others who are tackling social problems using a multitude of tools."
+  const goBeyondTechnology =
+    'Technology is only one tool we use in our greater mission for social impact. Technology alone is not enough. We learn from, work with, and are inspired by others who are tackling social problems using a multitude of tools.';
 
-  const developWithCare = "We build with others in mind. Empathy and compassion are crucial to serving our partner organizations and members. When we embark on projects, we work to deeply understand the people who we are helping."
+  const developWithCare =
+    'We build with others in mind. Empathy and compassion are crucial to serving our partner organizations and members. When we embark on projects, we work to deeply understand the people who we are helping.';
 
   return (
     <div className={styles.valuesCardsDiv}>
@@ -67,33 +75,52 @@ function ValueCardRow() {
 }
 
 function ExecBoard() {
-  const res = useAxios(process.env.REACT_APP_ROOT_URL + "/api/members?populate=avatar,componentRolesArr&filters[memberDisplayStatus][$eq]=Current Board Member", "GET", {});
-  const boardMembers = res.data ? res.data["data"] : [];
+  const res = useAxios(
+    process.env.REACT_APP_ROOT_URL +
+      '/api/members?populate=avatar,componentRolesArr&filters[memberDisplayStatus][$eq]=Current Board Member',
+    'GET',
+    {},
+  );
+  const boardMembers = res.data ? res.data['data'] : [];
 
   return (
     <div className={styles.execBoardDiv}>
       <h2>Executive Board</h2>
       <div className={styles.execBoardPhotos}>
-        {!boardMembers ? boardMembers :
-          boardMembers.map((item, index) => (
-            <Person key={index}
-              memberName={item["attributes"]["firstName"] + ' ' + item["attributes"]["lastName"]}
-              role={(item['attributes']['componentRolesArr'] as Array<any>).find(e => e['isDisplayRole'] == true)['title']}
-              pronouns={item["attributes"]["pronouns"]}
-              src={item["attributes"]["avatar"]["data"] ? item["attributes"]["avatar"]["data"]["attributes"]["url"] : null}
-            />
-          ))}
+        {!boardMembers
+          ? boardMembers
+          : boardMembers.map((item, index) => (
+              <Person
+                key={index}
+                memberName={item['attributes']['firstName'] + ' ' + item['attributes']['lastName']}
+                role={
+                  (item['attributes']['componentRolesArr'] as Array<any>).find((e) => e['isDisplayRole'] == true)[
+                    'title'
+                  ]
+                }
+                pronouns={item['attributes']['pronouns']}
+                src={
+                  item['attributes']['avatar']['data']
+                    ? item['attributes']['avatar']['data']['attributes']['url']
+                    : null
+                }
+              />
+            ))}
       </div>
     </div>
   );
 }
 
 function TeamMembers() {
-
-  // fetch data with axios, assign the array of members to members var 
+  // fetch data with axios, assign the array of members to members var
   // const res = useAxios(process.env.REACT_APP_ROOT_URL + "/api/members?populate=*", "GET", {});
-  const res = useAxios(process.env.REACT_APP_ROOT_URL + "/api/members?populate=avatar,componentRolesArr&filters[memberDisplayStatus][$eq]=Current Member", "GET", {});
-  const members = res.data ? res.data["data"] : [];
+  const res = useAxios(
+    process.env.REACT_APP_ROOT_URL +
+      '/api/members?pagination[page]=1&pagination[pageSize]=100&populate=avatar,componentRolesArr&filters[memberDisplayStatus][$eq]=Current Member',
+    'GET',
+    {},
+  );
+  const members = res.data ? res.data['data'] : [];
 
   // members.map(item => console.log(item["attributes"]["firstName"]));
 
@@ -101,17 +128,31 @@ function TeamMembers() {
     <div className={styles.teamMembersDiv}>
       <h2>Team Members</h2>
       <div className={styles.teamMembersPhotos}>
-        {!members ? members :
-        // render team members
-          members.map((item, index) => (
-            <Person key={index}
-              memberName={item["attributes"]["firstName"] + ' ' + item["attributes"]["lastName"]}
-              team={(item['attributes']['componentRolesArr'] as Array<any>).find(e => e['isDisplayRole'] == true)['team']}
-              role={(item['attributes']['componentRolesArr'] as Array<any>).find(e => e['isDisplayRole'] == true)['title']}
-              pronouns={item["attributes"]["pronouns"]}
-              src={item["attributes"]["avatar"]["data"] ? item["attributes"]["avatar"]["data"]["attributes"]["url"] : null}
-            />
-          ))}
+        {!members
+          ? members
+          : // render team members
+            members.map((item, index) => (
+              <Person
+                key={index}
+                memberName={item['attributes']['firstName'] + ' ' + item['attributes']['lastName']}
+                team={
+                  (item['attributes']['componentRolesArr'] as Array<any>).find((e) => e['isDisplayRole'] == true)[
+                    'team'
+                  ]
+                }
+                role={
+                  (item['attributes']['componentRolesArr'] as Array<any>).find((e) => e['isDisplayRole'] == true)[
+                    'title'
+                  ]
+                }
+                pronouns={item['attributes']['pronouns']}
+                src={
+                  item['attributes']['avatar']['data']
+                    ? item['attributes']['avatar']['data']['attributes']['url']
+                    : null
+                }
+              />
+            ))}
       </div>
     </div>
   );
