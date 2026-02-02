03aa7328935b4296a46beb9d52b65032a8d3221a
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2026-02-02 15:17:23 -0500

    chore: use ESM import for tailwindcss-animate

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: [
@@ -105,6 +107,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
-
